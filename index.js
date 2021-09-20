15b86f5d0d5dc123a3de45461f16684da481a41d
Alexandre Pilleyre <alexandre.pilleyre@grenoble-inp.org>
2021-09-20 15:42:42 +0200

    Initial commit with very basic files.
    This commit provides some very basic files for a HTML+CSS+JS project.
    The strict mode is currently enabled for the JS script.

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+
+function main() {
+    console.log('Hello world.');
+}
+
+// Use window.onload event to launch the main function when loading process has ended
+window.onload = main;
